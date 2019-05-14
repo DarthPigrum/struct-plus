24b2df8cddcc9e72436684bf8f8af04ea241a87c
DarthPigrum <43322390+DarthPigrum@users.noreply.github.com>
2019-05-15 01:47:18 +0300

    Tests for float, double and char

--- tests/test.js
+++ tests/test.js
@@ -63,8 +63,61 @@ const testIntArrayTypes = (types) => {
     i++;
   }
 };
+const testFloatTypes = (types) => {
+  const arr = [];
+  let i = 0;
+  for (const type of types) {
+    const name = names[i % names.length] + i;
+    arr.push([type, i + name], [type, name, i + 2]);
+    i++;
+  }
+  const description = struct(...arr);
+  const instance = new Struct(description);
+  i = 0;
+  for (const type of types) {
+    const name = names[i % names.length] + i;
+    const value = 1 / Math.pow(2, type.size);
+    instance.data[i + name] = value;
+    instance.data[name][i] = value * 2;
+    i++;
+  }
+  i = 0;
+  for (const type of types) {
+    const name = names[i % names.length] + i;
+    const value = 1 / Math.pow(2, type.size);
+    assert.strictEqual(instance.data[i + name], value,
+      'float read/write failed');
+    assert.strictEqual(instance.data[name][i], value * 2,
+      'float array read/write failed');
+    i++;
+  }
+};
+const testChar = type => {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const length = alphabet.length;
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    const name = names[i % names.length] + i;
+    arr.push([type(i + 2), name]);
+  }
+  const description = struct(...arr);
+  const instance = new Struct(description);
+  for (let i = 0; i < length; i++) {
+    const name = names[i % names.length] + i;
+    const value = alphabet.substring(0, i + 1);
+    instance.data[name] = value;
+  }
+  for (let i = 0; i < length; i++) {
+    const name = names[i % names.length] + i;
+    const value = alphabet.substring(0, i + 1) + '\0';
+    assert.strictEqual(instance.data[name], value,
+      'string read/write failed');
+  }
+};
 testIntTypes([types.int8, types.int16, types.int32, types.int48,
   types.uint8, types.uint16, types.uint32, types.uint48]);
 testIntArrayTypes([types.int8, types.int16, types.int32, types.int48,
   types.uint8, types.uint16, types.uint32, types.uint48]);
+testFloatTypes([types.float, types.double]);
+testChar(types.char);
 console.log('passed');
